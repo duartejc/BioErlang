4868e425215ac0878dc7e2e43c087ebae43f41d5
Jean Duarte <jeancduarte@gmail.com>
2014-03-12 01:43:51 -0300

    first commit

---
+++ string_algorithms/strings.erl
@@ -0,0 +1,14 @@
+-module(strings).
+-export([dna/1]).
+
+count_char(String, Chr) ->
+    F = fun(X, N) when X =:= Chr -> N + 1;
+           (_, N)                -> N
+        end,
+    lists:foldl(F, 0, String).
+
+%%Counting DNA Nucleotides
+dna(String) ->
+    {A, C, G, T} = {count_char(String, $A), count_char(String, $C), count_char(String, $G), count_char(String, $T)}.
+
+
